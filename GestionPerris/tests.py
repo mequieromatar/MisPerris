5bac32e45e8b38ac04f7d8666302ef48475f75c8
mequieromatar <44039800+mequieromatar@users.noreply.github.com>
2018-11-08 02:12:47 -0300

    Pruebas?
    se intento hacer las pruebas unitarias

--- GestionPerris/tests.py
+++ GestionPerris/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from GestionPerris.models import *
+# Create your tests here
 
-# Create your tests here.
+class perroTestCase(TestCase):
+
+    def setUp(self):
+        perro.objects.create(foto="perro-collie-corriendo.jpg",nombre_perro="Guau", raza_predominante="Shiba inu",descripcion="descripcion",estado="A")
+
+    def test_perritos_nombre():
+        perri = perro.objects.get(nombre_perro="Guau")
+        self.assertEqual(perri.perro.descripcion="descripcion")
